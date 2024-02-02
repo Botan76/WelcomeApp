e65b8fed2f94b8909699ebf68ea5db41d93db790
Botan Majeed <botan_art@hotmail.com>
2024-02-03 00:21:50 +0100

    final

--- app/src/main/java/com/bootan/welcomeapp/MainActivity.kt
+++ app/src/main/java/com/bootan/welcomeapp/MainActivity.kt
@@ -8,10 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val welcomeFragment = StartFragment()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, welcomeFragment)
-            .commit()
+
     }
 
 
