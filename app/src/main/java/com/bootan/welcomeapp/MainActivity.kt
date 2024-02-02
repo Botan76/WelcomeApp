1190d1020ad85cafde28b22eb1287f274a13366c
Botan Majeed <botan_art@hotmail.com>
2024-02-02 23:23:58 +0100

    Initial commit

---
+++ app/src/main/java/com/bootan/welcomeapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.bootan.welcomeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val welcomeFragment = WelcomeFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, welcomeFragment)
+            .commit()
+    }
+
+
+}
